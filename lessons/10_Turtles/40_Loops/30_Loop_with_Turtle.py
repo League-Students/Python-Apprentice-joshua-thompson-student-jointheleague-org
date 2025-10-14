a43bb0f81c5e3a6b01d96b39c5c5fd848a59489f
League Student <student@jointheleague.org>
2025-10-14 15:33:02 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/40_Loops/30_Loop_with_Turtle.py
+++ lessons/10_Turtles/40_Loops/30_Loop_with_Turtle.py
@@ -4,12 +4,15 @@ Turtles with a loop.
 Study the previous program, 05a_Loop_with_Turtle.py, and then
 write a new program that uses a loop to draw a pentagon.
 ( You can cut and past most of it! )
-
-impo
-
-
+"""
 
 ... # Your code here
+import turtle
+tina = turtle.Turtle()
+tina.shape = 
+
+
+
 for i in range(6):
     
 
